ad82cca67ff827548d6b00c7359f88678e906cb0
GitHubJIYEON <jiyeon2954@gmail.com>
2025-03-20 15:11:24 +0900

    fix: id = app -> root로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import Content from "./components/Content";
 import Header from "./components/Header";
 
 function App() {
-    return (
-        <div id="app">
-            <Header />
-            <Content />
-        </div>
-    );
+  return (
+    <div id="app">
+      <Header />
+      <Content />
+    </div>
+  );
 }
 export default App;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import App from "./App.jsx";
+import { render } from "./libs/jsx/render.js";
 
+const root = document.getElementById("root");
 const appElement = App();
 
-console.log(JSON.stringify(appElement, null, 2)); //virtual DOM 생성 확인
+console.log("App()이 반환한 Virtual DOM:", appElement);
+
+render(appElement, root); //실제 DOM 렌더링
+console.log("렌더링 후 root.innerHTML:", root.innerHTML);
+
+// console.log(JSON.stringify(appElement, null, 2)); //virtual DOM 생성 확인
